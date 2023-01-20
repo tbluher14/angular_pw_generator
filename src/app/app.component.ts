46a0979b71e5163e9cadda1b18e78f4a8f69478b
Tom Bluher <tbluher14@gmail.com>
2023-01-20 12:18:49 -0700

    managing state in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
-
+  includeLetters = false;
   // alternatively can be written as:
   // constructor() {
   //   this.password = '';
   // }
 
-  
+
   onButtonClick(){
     this.password = 'dis my pathword';
   }
 
-
+  onChangeUseLetters(){
+    // Changes the boolean to the opposite of what it is
+    this.includeLetters = !this.includeLetters;
+  }
 }
