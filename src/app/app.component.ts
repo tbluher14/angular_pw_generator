5af25f7404da05782a6d7efc264a55a317d49b29
Tom Bluher <tbluher14@gmail.com>
2023-01-20 12:22:42 -0700

    added event listeners to all check boxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  password = '';
-  includeLetters = false;
   // alternatively can be written as:
   // constructor() {
   //   this.password = '';
   // }
-
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
 
   onButtonClick(){
     this.password = 'dis my pathword';
+    console.log(this.includeLetters)
   }
 
   onChangeUseLetters(){
     // Changes the boolean to the opposite of what it is
     this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
   }
 }
